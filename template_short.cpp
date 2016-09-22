cc7e38e4dae86717092f9dcf7827a2b1bbf0e6f5
partha pritam mahanta <parthapritam2717@hotmail.com>
2016-09-22 17:54:24 +0530

    New Short template cpp

---
+++ template_short.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+typedef vector<string> vs;
+typedef unordered_map<int,int> umap;
+typedef map<long long,long long> omap;
+typedef multimap<long long,long long> mmap;
+
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define all(v) v.begin(),v.end()
+#define Pi(x) printf("%d", x)
+#define Pl(x) printf("%lld", x)
+#define os() printf(" ")
+#define nl() printf("\n");
+#define F first
+#define S second
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define PI 3.14159265
+#define INF 8944674407370955161LL
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--){
+    	
+    	
+    	
+    	
+    	
+    }
+    return 0;
+}
+
