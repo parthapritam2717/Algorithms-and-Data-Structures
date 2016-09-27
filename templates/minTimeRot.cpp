d60e93951b3eed0b1121c8ff85d60bf878176189
partha.lab <parthapritam2717@hotmail.com>
2016-09-28 01:47:59 +0530

    rot apple partial done

---
+++ templates/minTimeRot.cpp
@@ -0,0 +1,70 @@
+/*This program calculates the amount of time required to rot apples , rotten apple can rot adjacent good apples in 1 second  1 adjacent apple in any of the 4
+direction.
+2- rotten
+1- fresh
+0- no apple*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+typedef vector<string> vs;
+typedef unordered_map<int,int> umap;
+typedef map<long long,long long> omap;
+typedef multimap<long long,long long> mmap;
+
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define all(v) v.begin(),v.end()
+#define Pi(x) printf("%d", x)
+#define Pl(x) printf("%lld", x)
+#define os() printf(" ")
+#define nl() printf("\n");
+#define F first
+#define S second
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define PI 3.14159265
+#define INF 8944674407370955161LL
+int main()
+{
+    //std::ios::sync_with_stdio(false);
+    int r,c;
+    cin>>r>>c;
+    vvi arr(r,vi (c,0));
+    for(int i=0;i<r;++i){
+        for(int j=0;j<c;++j){
+            cin>>arr[i][j];
+        }
+    }
+    // graph input over
+    int ans=0;
+    bool flag=true;// to make sure that we only increase and for a given level at most once
+    /*Apply level order traversal with bfs and at each level we will keep on adding the neighboring apples*/
+    queue<pii> q;// to perform bfs
+    // find all rotten apples and add them to the queue
+    for(int i=0;i<r;++i){
+        for(int j=0;j<c;++j){
+            if(arr[i][j]==2){
+                q.push(mp(i,j));
+            }
+        }
+    }
+    // now we will insert a delim
+
+
+    return 0;
+}
+
+
